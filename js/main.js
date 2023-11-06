9c9bfab75c694c46183ca428795e9d92dddd895c
Ricardo <ricardo.scanavino96@gmail.com>
2023-11-05 21:56:21 -0300

    se agregaron las validaciones

--- js/main.js
+++ js/main.js
@@ -6,11 +6,20 @@ const calcularPromedioEdad = () => {
 
     let cantidadPersonas = parseInt(prompt("ingrese la cantidad de personas que desea calcular el promedio de edad"))
 
+    while (isNaN(cantidadPersonas)) {
+        cantidadPersonas = parseInt(prompt("ingrese la cantidad de personas que desea calcular el promedio de edad. Solo se aceptan numeros"));
+    }
+
     let totalEdades = 0
 
     for(i=0; i < cantidadPersonas; i++){
-        let edad = parseInt(prompt("ingrese su edad"))
 
+        let edad = parseInt(prompt("ingrese su edad"))
+        while (isNaN(edad)) {
+            edad = parseInt(prompt("ingrese su edad, solo se aceptan numeros"));
+        }
+        
+        
         totalEdades=totalEdades+edad
 
     }
